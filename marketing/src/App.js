5fa0245355099e5c1e852d1007596713441f1b99
Firooze Javaheri <firooze.javaheri@gmail.com>
2023-05-13 23:16:11 +0200

    feat(marketing): add navigation sync with container

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,29 +1,16 @@
-import React from 'react';
-import { Switch, Route, Router } from 'react-router-dom';
-import {
-  StylesProvider,
-  createGenerateClassName,
-} from '@material-ui/core/styles';
+import React from "react"
+import { Switch, Route, Router } from "react-router-dom"
+import Landing from "./components/Landing"
+import Pricing from "./components/Pricing"
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
-const generateClassName = createGenerateClassName({
-  productionPrefix: 'ma',
-});
-
-export default ({ history }) => {
-  return (
-    <div>
-      <StylesProvider>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/pricing" component={Pricing} />
-            <Route path="/" component={Landing} />
-          </Switch>
-        </BrowserRouter>
-      </StylesProvider>
-    </div>
-  );
-};
+export default ({history}) => {
+    return (
+        <Router history={history}>
+            <Switch>
+                <Route exact path="/pricing" component={Pricing} />
+                <Route path="/" component={Landing} />
+            </Switch>
+        </Router>
+    )
+}
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,17 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react"
+import ReactDOM from "react-dom"
+import { createMemoryHistory, createBrowserHistory } from "history"
+import App from "./App"
 
-const mount = (el) => {
-    console.log(el)
-    ReactDOM.render(<App />, el)
+const mount = (el, { onNavigate, defaultHistory, initialPath } = {}) => {
+    const history =
+        defaultHistory ||
+        createMemoryHistory({
+            initialEntries: [initialPath],
+        })
+
+    if (onNavigate) {
+        /**
+         * whenever memoryHistory changes history.listen calls the callback (onNavigate)
+         * as a result container will notice the navigation occured
+         */
+        history.listen(onNavigate)
+    }
+
+    ReactDOM.render(<App history={history} />, el)
+
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location
+
+            if (pathname !== nextPathname) {
+                history.push(nextPathname)
+            }
+        },
+    }
 }
 
-if(process.env.NODE_ENV === 'development') {
-    const devRoot = document.querySelector('#_marketing-dev-root');
+/**
+ * if this mfe is running in isolation (without container)
+ */
+if (process.env.NODE_ENV === "development") {
+    const devRoot = document.querySelector("#_marketing-dev-root")
 
     if (devRoot) {
-      mount(devRoot);
+        /**
+         * if mfe is running in isolation(without container), then use browser history
+         */
+        mount(devRoot, { defaultHistory: createBrowserHistory() })
     }
 }
 
