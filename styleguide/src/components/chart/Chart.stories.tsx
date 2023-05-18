d362a829cde98f52f7309a81c03830fd73924597
Firooze Javaheri <firooze.javaheri@gmail.com>
2023-05-18 21:43:35 +0200

    fix(styleguide): upgrade chart stories

--- styleguide/src/components/chart/Chart.stories.tsx
+++ styleguide/src/components/chart/Chart.stories.tsx
@@ -1,48 +1,47 @@
 import React from "react"
-
-import { ComponentStory, ComponentMeta } from "@storybook/react"
+import type { Meta, StoryObj } from "@storybook/react"
 import { Chart } from "./Chart"
 
-export default {
+const meta: Meta<typeof Chart> = {
     title: "Chart",
     component: Chart,
-} as ComponentMeta<typeof Chart>
+}
 
-//👇 We create a “template” of how args map to rendering
-const Template: ComponentStory<typeof Chart> = (args) => <Chart {...args} />
+export default meta
+type Story = StoryObj<typeof Chart>
 
-export const ExampleChart = Template.bind({})
-
-ExampleChart.args = {
-    options: {
-        xAxis: {
-            categories: [
-                "Jan",
-                "Feb",
-                "Mar",
-                "Apr",
-                "May",
-                "Jun",
-                "Jul",
-                "Aug",
-                "Sep",
-                "Oct",
-                "Nov",
-                "Dec",
-            ],
-        },
-        plotOptions: {
-            series: {
-                allowPointSelect: true,
-            },
-        },
-        series: [
-            {
-                data: [
-                    29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4,
-                    194.1, 95.6, 54.4,
+export const ExampleChart: Story = {
+    args: {
+        options: {
+            xAxis: {
+                categories: [
+                    "Jan",
+                    "Feb",
+                    "Mar",
+                    "Apr",
+                    "May",
+                    "Jun",
+                    "Jul",
+                    "Aug",
+                    "Sep",
+                    "Oct",
+                    "Nov",
+                    "Dec",
                 ],
             },
-        ],
+            plotOptions: {
+                series: {
+                    allowPointSelect: true,
+                },
+            },
+            series: [
+                {
+                    data: [
+                        29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5,
+                        216.4, 194.1, 95.6, 54.4,
+                    ],
+                },
+            ],
+        },
     },
 }
