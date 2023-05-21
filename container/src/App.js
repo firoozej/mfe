61c37c19f4a3dc7814cdce2ae4cab464d7778455
Firooze Javaheri <firooze.javaheri@gmail.com>
2023-05-21 12:23:35 +0200

    fix(*): fix workflow package issues

--- container/src/App.js
+++ container/src/App.js
@@ -1,25 +1,39 @@
-import React, { Suspense, lazy, useState } from "react"
-import { BrowserRouter, Switch, Route } from "react-router-dom"
+import React, { Suspense, lazy, useEffect, useState } from "react"
+import { Router, Switch, Route, Redirect } from "react-router-dom"
+import { createBrowserHistory } from "history"
 import Header from "./components/Header"
 const MarketingLazy = lazy(() => import("./components/MarketingApp"))
 const AuthLazy = lazy(() => import("./components/AuthApp"))
 //const DashboardLazy = lazy(() => import('./components/DashboardApp'));
 
+const history = createBrowserHistory()
+
 export default () => {
     const [isSignedIn, setIsSignedIn] = useState(false)
+
+    useEffect(() => {
+        if (isSignedIn) {
+            history.push("/dashboard")
+        }
+    }, [isSignedIn])
+
     return (
-        <BrowserRouter>
+        <Router history={history}>
             <Header isSignedIn={isSignedIn} />
             <Suspense fallback="...loading">
                 <Switch>
                     <Route path="/auth">
                         <AuthLazy onSignIn={() => setIsSignedIn(true)} />
                     </Route>
+                    <Route path="/dashboard">
+                        {!isSignedIn && <Redirect to="/" />}
+                        <>hey dashboard</>
+                    </Route>
                     <Route path="/">
                         <MarketingLazy isSignedIn={isSignedIn} />
                     </Route>
                 </Switch>
             </Suspense>
-        </BrowserRouter>
+        </Router>
     )
 }
